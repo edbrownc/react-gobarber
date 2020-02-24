589147d3eb778c265c6ef107a2e4da474a22ead2
Ed <ed@pop-os.localdomain>
2020-02-23 17:30:32 -0800

    Created header/notifications

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   email: Yup.string()
@@ -13,8 +17,11 @@ const schema = Yup.object().shape({
 });
 
 export default function SignIn() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
 
   return (
@@ -24,7 +31,7 @@ export default function SignIn() {
         <Input name="email" type="email" placeholder="Your email" />
         <Input name="password" type="password" placeholder="Your password" />
 
-        <button type="submit">Sign in</button>
+        <button type="submit">{loading ? 'Loading...' : 'Sign in'}</button>
         <Link to="/register">Sign up for free</Link>
       </Form>
     </>
